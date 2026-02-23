e8712bb1d4ec28cf271060dc28881ee9c938f2a3
Embrik <embrik.skrindo@gmail.com>
2026-02-23 19:24:27 +0100

    ci: deploy Next.js site to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "garnly";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ...(isGitHubPages
+    ? {
+        output: "export",
+        trailingSlash: true,
+        basePath: `/${repoName}`,
+        assetPrefix: `/${repoName}/`,
+        images: {
+          unoptimized: true,
+        },
+      }
+    : {}),
 };
 
 export default nextConfig;
